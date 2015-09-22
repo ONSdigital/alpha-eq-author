aee07f0c7fa8bc561331e54fa36e095004b5de28
David Jones <david@weaponsdivision.co.uk>
2015-09-22 15:50:51 +0100

    Plumbed together

--- author/urls.py
+++ author/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
-
-
+from author.views import LoginView, WelcomeView, AnotherView
 
 urlpatterns = patterns('',
-    url(r'^$',  TemplateView.as_view(template_name='index.html'), name='home'),
+    url(r'^login', LoginView.as_view(), name='login'),
+    url(r'^another', AnotherView.as_view()),
+
+    url(r'^$',  WelcomeView.as_view()),
 
     url(r'^admin/', include(admin.site.urls)),
 )
