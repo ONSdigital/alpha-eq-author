2f50abcdd2e2dd5c2f1b846307a322928ca44d06
Warren <warren@methodsdigital.co.uk>
2015-09-22 16:18:14 +0100

    DIGEQ-2 Adding tests for setup new survey

--- survey/tests.py
+++ survey/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Survey
 
-# Create your tests here.
+
+class SurveyTestCase(TestCase):
+    def setUp(self):
+        Survey.objects.create(title="Test Survey 1", survey_id="1")
+        Survey.objects.create(title="Test Survey 2", survey_id="2")
+
+    def test_survey(self):
+        survey1 = Survey.objects.get(survey_id="1")
+        survey2 = Survey.objects.get(survey_id="2")
+        self.assertEqual("Test Survey 1", survey1.title)
+        self.assertEqual("Test Survey 2", survey2.title)
+
+    def test_check_survey_count(self):
+        client = Client()
+        response = client.get('/surveys/')
+        self.assertEqual(len(response.context['object_list']), 2)
+
+    def test_add_survey(self):
+        client = Client()
+        response = client.post('/surveys/add/', {'survey_list': '024'}, follow=True)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(len(response.context['object_list']), 3)
+
+
