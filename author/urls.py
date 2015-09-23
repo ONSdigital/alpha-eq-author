e043e2e83d56fef28dd8bc35ae8ef77fb8f9d163
David Jones <david@weaponsdivision.co.uk>
2015-09-23 13:16:12 +0100

    Integrate latest UI files

--- author/urls.py
+++ author/urls.py
@@ -5,7 +5,6 @@ from author.views import LoginView, WelcomeView
 
 urlpatterns = patterns('',
     url(r'^login', LoginView.as_view(), name='login'),
-    url(r'^another', AnotherView.as_view()),
 
     url(r'^$',  WelcomeView.as_view(), name="welcome"),
 
