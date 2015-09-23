216451fe2f414ad7483773b0760639df34b34a60
David Jones <david@weaponsdivision.co.uk>
2015-09-23 12:50:13 +0100

    Working, with test

--- author/urls.py
+++ author/urls.py
@@ -2,9 +2,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from author.views import LoginView, WelcomeView
 
+
 urlpatterns = patterns('',
     url(r'^login', LoginView.as_view(), name='login'),
-    url(r'^$',  WelcomeView.as_view()),
+    url(r'^another', AnotherView.as_view()),
+
+    url(r'^$',  WelcomeView.as_view(), name="welcome"),
+
     url(r'^admin/', include(admin.site.urls)),
     url(r'^surveys/', include("survey.urls", namespace='survey')),
+
+    url(r'^logout', LogoutView.as_view(), name='logout')
 )
