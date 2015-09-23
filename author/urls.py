afd708382347cd0d388fe490386fe74374ad6ad9
Alex Morris <alexmorris@me.com>
2015-09-23 16:21:50 +0100

    initial header markup and base css for create page

--- author/urls.py
+++ author/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = patterns('',
+    url(r'^create/$',  TemplateView.as_view(template_name='create.html'), name='create'),
     url(r'^$',  TemplateView.as_view(template_name='index.html'), name='home'),
-
     url(r'^admin/', include(admin.site.urls)),
 )
