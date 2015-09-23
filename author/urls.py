38353ef573c46a9a8001990782c238b1c1879162
David Jones <david@weaponsdivision.co.uk>
2015-09-23 12:50:13 +0100

    Working, with test

--- author/urls.py
+++ author/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from author.views import LoginView, WelcomeView, AnotherView
+from author.views import LoginView, WelcomeView, AnotherView, LogoutView
 
 urlpatterns = patterns('',
     url(r'^login', LoginView.as_view(), name='login'),
     url(r'^another', AnotherView.as_view()),
 
-    url(r'^$',  WelcomeView.as_view()),
+    url(r'^$',  WelcomeView.as_view(), name="welcome"),
 
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^logout', LogoutView.as_view(), name='logout')
 )
--- author/views.py
+++ author/views.py
@@ -1,6 +1,10 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, logout, login
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.core.urlresolvers import reverse
+
 
 class LoginRequiredMixin(object):
     @classmethod
@@ -8,15 +12,34 @@ class LoginRequiredMixin(object):
         view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
         return login_required(view)
 
+
 class WelcomeView(LoginRequiredMixin, TemplateView):
     template_name = 'welcome.html'
 
+
 class AnotherView(LoginRequiredMixin, TemplateView):
     template_name = 'index.html'
+
 
 class LoginView(TemplateView):
     template_name = 'login.html'
 
     def post(self, request):
-        return HttpResponse('Hello %s' % request.POST['username'])
 
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            # password is verified
+            if user.is_active:
+                login(request, user)
+                if 'next' in request.GET:
+                    return redirect(request.GET['next'])
+
+                return redirect(reverse('welcome'))
+
+        return HttpResponse('Sorry %s, your name\'s not down, you\'re not coming in.' % request.POST['username'])
+
+
+class LogoutView(TemplateView):
+    def get(self, request):
+        logout(request)
+        return redirect(reverse('welcome'))
