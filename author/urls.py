9bceebea607735ed4fb5a9e06051dc8eded3b5ce
David Jones <david@weaponsdivision.co.uk>
2015-10-01 15:38:36 +0100

    Ajax login screen

--- author/urls.py
+++ author/urls.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from author.views import WelcomeView
 from django.views.generic import TemplateView
+from .views import login
 
 urlpatterns = patterns('',
     url(r'^surveys/', include("survey.urls", namespace='survey')),
-    url(r'^login/$', auth_views.login, {'template_name':'login.html'}),
+    url(r'^login/$', login, {'template_name':'login.html'}),
     url(r'^create/$',  TemplateView.as_view(template_name='create.html'), name='create'),
     url(r'^home/$',  TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^add/$',  TemplateView.as_view(template_name='add.html'), name='add'),
