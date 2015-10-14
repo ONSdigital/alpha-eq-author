b6cdddf015dd10b11f9f975ac96e1caa73de6bb5
David Jones <david@weaponsdivision.co.uk>
2015-10-14 16:01:03 +0100

    Prevents contenteditable from being used as drop taregt

--- author/static/js/q_builder/directives.js
+++ author/static/js/q_builder/directives.js
@@ -11,6 +11,9 @@
               }
 
               ngModel.$render = function() {
+                // Do not allow drag and drop into contenteditable
+                element.attr("ondragenter", "event.preventDefault(); event.dataTransfer.dropEffect = 'none'");
+                element.attr("ondragover", "event.preventDefault(); event.dataTransfer.dropEffect = 'none'");
                 element.html(ngModel.$viewValue || "");
               };
 
