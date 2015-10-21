c043fd7c1420900348cf73f3bd02ac127560342c
Alex Morris <alexmorris@me.com>
2015-10-21 15:53:55 +0100

    added deletion confirm and fade animation

--- author/static/js/q_builder/app.js
+++ author/static/js/q_builder/app.js
@@ -1,14 +1,16 @@
 (function() {
-    angular.module("QBuilder", [
-        'QBuilderConfig',
-        'ngSanitize',
-        'dndLists',
-        'contenteditable',
-        'wysiwygEditor',
-        'tabs',
-        'BuilderController',
-        'RadioQuestionController'
-    ]);
+  angular.module("QBuilder", [
+    'QBuilderConfig',
+    'ngSanitize',
+    'dndLists',
+    'contenteditable',
+    'wysiwygEditor',
+    'tabs',
+    'BuilderController',
+    'RadioQuestionController',
+    'ngAnimate',
+    'confirmdialog'
+  ]);
 
-    // See config.js, and controllers/*.js
+  // See config.js, and controllers/*.js
 })();
