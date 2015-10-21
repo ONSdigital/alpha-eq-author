919f40693d0d17cb092ddf3e3c4af4d272595fd9
Alex Morris <alexmorris@me.com>
2015-10-21 17:18:33 +0100

    updated RTE config to show relevant formatting options

--- author/static/js/q_builder/directives/WysiwygEditor.js
+++ author/static/js/q_builder/directives/WysiwygEditor.js
@@ -1,26 +1,29 @@
 (function() {
-    angular.module('wysiwygEditor', [])
-        .directive('wysiwygEditor', function($timeout) {
-          return {
-            restrict: 'A',
-            require: 'ngModel',
-            link: function(scope, element, attrs, ngModel) {
-              // Start the editor
-              Simditor.locale = 'en_US';
-              element.editor = new Simditor({
-                textarea: $(element)
-              });
+  angular.module('wysiwygEditor', [])
+    .directive('wysiwygEditor', function($timeout) {
+      return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModel) {
+          // Start the editor
+          Simditor.locale = 'en_US';
+          toolbar = ['bold', 'italic', 'underline', 'ol', 'ul'];
+          element.editor = new Simditor({
+            textarea: $(element),
+            toolbar: toolbar
+          });
 
-              element.editor.on('valuechanged', function(e) {
-                  $timeout(function() {
-                    ngModel.$setViewValue(element.editor.getValue());
-                  });
-              });
+          element.editor.on('valuechanged', function(e) {
+            $timeout(function() {
+              ngModel.$setViewValue(element.editor.getValue());
+            });
+          });
 
-              ngModel.$render = function() {
-                element.editor.setValue(ngModel.$viewValue).trigger('change');
-              }
-            }
-          };
-        });
+          ngModel.$render = function() {
+            element.editor.setValue(ngModel.$viewValue).trigger(
+              'change');
+          }
+        }
+      };
+    });
 })();
