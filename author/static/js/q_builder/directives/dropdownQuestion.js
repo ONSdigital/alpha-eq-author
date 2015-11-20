8dd86833f4d14b5f6beaac86ea5ac3fdb4b7a31e
Warren Bailey <warren@methodsdigital.co.uk>
2015-11-20 09:46:26 +0000

    Reverting formatting changes in dropdownQuestions.js

--- author/static/js/q_builder/directives/dropdownQuestion.js
+++ author/static/js/q_builder/directives/dropdownQuestion.js
@@ -3,9 +3,9 @@
 
   .directive("dropdownQuestion", function() {
     return {
-      restrict: 'E',
-      templateUrl: '/static/js/q_builder/templates/dropdown_question.html',
-      controller: function($scope, $http) {
+        restrict: 'E',
+        templateUrl: '/static/js/q_builder/templates/dropdown_question.html',
+        controller: function($scope, $http) {
 
         /**
          *  load available json list imports into the ui dropdown
@@ -13,44 +13,46 @@
 
         $http.get('/static/js/q_builder/json/jsonImportList.json').
         success(function(data) {
-          $scope.jsonImportList = data;
+            $scope.jsonImportList = data;
         });
 
         /**
          *  load the json list into the questionnaire json
          */
 
-        $scope.load = function(jsonImport, question) {
+        $scope.load = function(jsonImport,question) {
 
-          if (jsonImport != null) {
-            $http.get('/static/js/q_builder/json/' + jsonImport +
-              '.json').
-            success(function(data) {
-              angular.element('.status').addClass('success');
-              question.parts = [];
-              for (var key in data.elements) {
+            if (jsonImport != null){
+                $http.get('/static/js/q_builder/json/' + jsonImport +'.json').
+                    success(function(data) {
+                        angular.element('.status').addClass('success');
+                        question.parts = [];
+
+                        for (var key in data.elements){
+                            question.parts.push({
+                            type: 'option',
+                            value: data.elements[key].value
+                            });
+                        }
+                    });
+                };
+            };
+            /**
+             *  Add an answer option
+             */
+            $scope.addOption = function(question) {
+                if (typeof question.parts == "undefined" ) {
+                    question.parts = [];
+                }
                 question.parts.push({
-                  type: 'option',
-                  value: data.elements[key].value
+                    type: 'option',
+                    value: ''
                 });
-              }
-            });
-          };
+            };
+       },
+    controllerAs: 'ctrl'
         };
-        /**
-         *  Add an answer option
-         */
-        $scope.addOption = function(question) {
-          if (typeof question.parts == "undefined") {
-            question.parts = [];
-          }
-          question.parts.push({
-            type: 'option',
-            value: ''
-          });
-        };
-      },
-      controllerAs: 'ctrl'
-    };
-  });
+    });
 })();
+
+
