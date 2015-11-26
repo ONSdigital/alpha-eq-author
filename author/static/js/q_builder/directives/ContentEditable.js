f6de4ea99fc580b9c4311130b12911f1c34e892e
Alex Morris <alexmorris@me.com>
2015-11-26 14:40:53 +0000

    freebie fix for weird characters in contenteditables especially for @collisdigital

--- author/static/js/q_builder/directives/ContentEditable.js
+++ author/static/js/q_builder/directives/ContentEditable.js
@@ -7,7 +7,7 @@
         link: function(scope, element, attrs, ngModel) {
 
           function read() {
-            ngModel.$setViewValue(element.html());
+            ngModel.$setViewValue(element.text());
           }
 
           ngModel.$render = function() {
