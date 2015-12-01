2919a62a9901c2962aecb4386aae4262eef22870
Lewis Babbage <lewis.babbage@ons.gsi.gov.uk>
2015-12-01 18:15:22 +0000

    digeq-170 section headings, if clear out unnamed section onclick

--- author/static/js/q_builder/directives/questionGroup.js
+++ author/static/js/q_builder/directives/questionGroup.js
@@ -4,7 +4,18 @@
   .directive("questionGroup", function() {
     return {
         restrict: 'E',
-        templateUrl: '/static/js/q_builder/templates/question_group.html'
+        templateUrl: '/static/js/q_builder/templates/question_group.html',
+         controller: function($scope) {
+            /**
+             *  Clear unnamed section title
+             */
+            $scope.initSection = function(item) {
+               if (item.questionText =='Unnamed Section'){
+                item.questionText ='';
+                }
+            };
+        },
+        controllerAs: 'ctrl'
         };
     });
 })();
