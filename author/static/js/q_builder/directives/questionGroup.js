bdd4c9b6e9f7a5bcd3816539b9db81d8d1d29525
Lewis Babbage <lewis.babbage@ons.gsi.gov.uk>
2015-12-02 09:45:23 +0000

    digeq-170 section headings, insert placeholder if text is left empty

--- author/static/js/q_builder/directives/questionGroup.js
+++ author/static/js/q_builder/directives/questionGroup.js
@@ -6,12 +6,23 @@
         restrict: 'E',
         templateUrl: '/static/js/q_builder/templates/question_group.html',
          controller: function($scope) {
+
             /**
              *  Clear unnamed section title
              */
             $scope.initSection = function(item) {
+
                if (item.questionText =='Unnamed Section'){
                 item.questionText ='';
+                }
+            };
+
+             /**
+             *  insert placeholder if empty
+             */
+               $scope.emptyHeader = function(item) {
+               if (item.questionText ==''){
+                item.questionText ='Unnamed Section';
                 }
             };
         },
